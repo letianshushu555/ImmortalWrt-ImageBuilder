15e69971e044b2418db535b16bf23f31ebf228eb
letianshushu555 <yingming.yan@gmail.com>
2026-06-01 17:29:26 +0800

    Update build25.sh

--- sunxi-cortexa53/build25.sh
+++ sunxi-cortexa53/build25.sh
@@ -55,6 +55,15 @@ PACKAGES="$PACKAGES luci-theme-argon"
 PACKAGES="$PACKAGES luci-app-argon-config"
 PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
+PACKAGES="$PACKAGES luci-app-passwall"
+PACKAGES="$PACKAGES luci-app-netdata"
+PACKAGES="$PACKAGES luci-app-openclash"
+PACKAGES="$PACKAGES luci-app-daed"
+PACKAGES="$PACKAGES luci-app-adbyby-plus"
+PACKAGES="$PACKAGES luci-app-vlmcsd"
+PACKAGES="$PACKAGES luci-app-ddnsto"
+PACKAGES="$PACKAGES luci-app-ttyd"
+
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
     PACKAGES="$PACKAGES luci-i18n-dockerman-zh-cn"
